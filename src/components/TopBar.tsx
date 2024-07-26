0ec902b963c0b9e5adc4abf80b159b656b7db8ff
Медведев Ваня <ranzar2000@gmail.com>
2024-07-26 22:56:29 +0300

    Add TopBar component with authentication handling
    This commit introduces the TopBar component, which uses Material UI for the UI, Recoil for state management, and a custom authentication hook. It includes a logout button that appears only when the user is authenticated and handles logging out and navigation.

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,32 @@
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material"
+import { useRecoilValue } from "recoil";
+import { authState } from "../state/authState";
+import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
+const TopBar: React.FC = () => {
+    const auth = useRecoilValue(authState);
+    const { logout } = useAuth();
+    const router = useNavigate();
+
+
+    const handleLogout = () => {
+        logout();
+        router("/");
+    };
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Кондуктор
+                    </Typography>
+                    {auth.isAuthenticated ? <Button color="inherit" onClick={() => handleLogout()}>Выйти</Button> : null}
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+
+export default TopBar;
