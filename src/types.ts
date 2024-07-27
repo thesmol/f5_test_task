396207f8dad106f6492c38074382de103a911299
Медведев Ваня <ranzar2000@gmail.com>
2024-07-27 03:36:10 +0300

    Add ag-grid-community
    Package for the table theme

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,23 @@
 export interface Product {
-    id: number,
-    name: string,
+    id: number;
+    name: string;
     company: {
-        name: string
-    },
-    defaultBuyPrice: number,
-    defaultSellPrice: number
+        name: string,
+    };
+    defaultBuyPrice: number;
+    defaultSellPrice: number;
 }
 
 export interface User {
-    email: string
-    phone?: string
-    name: string
-    password: string
-    userRoleId: number
-    leaders: [number]
+    email: string;
+    phone?: string;
+    name: string;
+    password: string;
+    userRoleId: number | null;
+    leaders: [number] | [];
+}
+
+export interface Role {
+    id: number;
+    name: string;
 }
