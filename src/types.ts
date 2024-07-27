6d6f5ef77907ef48687d8ab8a21b94bdb8e78853
Медведев Ваня <ranzar2000@gmail.com>
2024-07-27 03:39:58 +0300

    Make types look prettier

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,19 @@
 export interface Product {
-    id: number,
-    name: string,
+    id: number;
+    name: string;
     company: {
-        name: string
-    },
-    defaultBuyPrice: number,
-    defaultSellPrice: number
+        name: string,
+    };
+    defaultBuyPrice: number;
+    defaultSellPrice: number;
 }
 
 export interface User {
-    email: string
-    phone?: string
-    name: string
-    password: string
-    userRoleId: number
-    leaders: [number]
+    email: string;
+    phone?: string;
+    name: string;
+    password: string;
+    userRoleId: number | null;
+    leaders: [number] | [];
+}
 }
