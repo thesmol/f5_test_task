239db0403e3137cfc0adbcca10dcc30704faad57
Медведев Иван <ranzar2000@gmail.com>
2024-08-07 18:51:12 +0300

    Add top margin for the container buttons

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -9,7 +9,7 @@ import ProductsTable from "../components/ProductsTable"
  */
 const DashboardPage: React.FC = () => {
     return (
-        <Container maxWidth="md" className="w-full h-fit max-h-screen">
+        <Container maxWidth="md" className="w-full h-fit max-h-screen" sx={{ '& button': { mt: 3 } }}>
             <Typography variant="h4" marginBottom={2} marginTop={4}>
                 Продукты
             </Typography>
