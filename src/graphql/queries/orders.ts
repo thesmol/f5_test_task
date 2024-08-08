5e1d5d0f53d6b8f00a225116e4dcd4694175ba49
Медведев Иван <ranzar2000@gmail.com>
2024-08-08 20:20:21 +0300

    Make a quary for fetching order printing info

---
+++ src/graphql/queries/orders.ts
@@ -0,0 +1,37 @@
+import { gql } from '@apollo/client';
+
+/**
+ * Запрос для получения данных по конкретному заказу
+ */
+export const GET_PRINT_ORDER_BY_ID = gql`
+  query SalesOrder($id: Int!) {
+  salesOrder(id: $id) {
+    name
+    finishByDate
+    shippingAddress
+    billingAddress
+    info
+    archived
+    orderReadyDate
+    customer {
+      name
+    }
+    manager {
+      name
+    }
+  }
+  salesOrderTableData(id: $id) {
+    product {
+      name
+      archived
+      process {
+        unit {
+          name
+        }
+      }
+    }
+    quantity
+    pricePerUnit
+  }
+}
+`;
